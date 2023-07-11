1269c91be9a6f766c42638f9c954feeb36dd3cf4
meera <meerasandiya.a8@gmail.com>
2023-07-11 16:19:00 +0530

    Project Completed

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const btnNavEL = document.querySelector(".btn-mobile-nav");
 const headerEL = document.querySelector(".main-header");
 
 btnNavEL.addEventListener("click", function () {
+  document.body.classList.toggle('no-scroll')
   headerEL.classList.toggle("nav-open");
 });
+
+// Mobile Dropdown footer
